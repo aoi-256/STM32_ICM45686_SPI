411da436f8a29f61cfdc17f081ce05700bd84a2c
aoi-256 <aoi256jp@Gmail.com>
2025-03-06 11:19:16 +0900

    修正: Write()のREGのビット演算を修正

--- ICM45686_HAL.cpp
+++ ICM45686_HAL.cpp
@@ -16,7 +16,7 @@ ICM45686_HAL::ICM45686_HAL(SPI_HandleTypeDef *SPI_Pin){
 
 void ICM45686_HAL::Write(ICM45686_HAL::REGISTER REGISTER_ADDR, uint8_t* Command, uint8_t Len){
 
-	uint8_t REG = uint8_t(REGISTER_ADDR) & 0x7F;
+	uint8_t REG = uint8_t(REGISTER_ADDR) | 0x00;
 	HAL_SPI_TransmitReceive(&hspi1, &REG, Command, Len, 100);
 }
 
