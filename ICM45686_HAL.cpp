dbe4a56149e16c0113637dfcb24fd4d5455eda2b
aoi-256 <aoi256jp@Gmail.com>
2025-03-07 18:02:53 +0900

    fix: SPI通信への仮対応
    コンストラクタ未設定

--- ICM45686_HAL.cpp
+++ ICM45686_HAL.cpp
@@ -1,11 +1,11 @@
-/*
- * ICM45686_HAL.cpp
+/* * ICM45686_HAL.cpp
  *
  *  Created on: Dec 17, 2024
  *      Author: aoi25
  */
 
 #include <ICM45686_HAL.h>
+#include "gpio.h"
 
 ICM45686_HAL::ICM45686_HAL(SPI_HandleTypeDef *SPI_Pin){
 
@@ -16,20 +16,38 @@ ICM45686_HAL::ICM45686_HAL(SPI_HandleTypeDef *SPI_Pin){
 
 void ICM45686_HAL::Write(ICM45686_HAL::REGISTER REGISTER_ADDR, uint8_t* Command, uint8_t Len){
 
-	uint8_t REG = uint8_t(REGISTER_ADDR) | 0x00;
-	
-	//書き込み先のレジスタを指定
-	HAL_SPI_Transmit(&hspi1, &REG, 1, 1000);
-	//データを書き込み
-    	HAL_SPI_Transmit(&hspi1, Command, Len, 1000);
+	uint8_t TxBuffer[2] = {};
+
+	for(uint8_t i=0; i<Len; i++){
+
+	TxBuffer[0] = ((uint8_t)REGISTER_ADDR + i) | 0x00;
+	TxBuffer[1] = Command[i];
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
+    HAL_SPI_Transmit(&hspi1, TxBuffer, 2, 1000);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+
+	}
 }
 
 void ICM45686_HAL::Read(ICM45686_HAL::REGISTER REGISTER_ADDR, uint8_t* Buffer, uint8_t Len){
 
-	uint8_t REG = uint8_t(REGISTER_ADDR) | 0x80;
-	//読み取りたいレジスタを指定
-	HAL_SPI_Transmit(ICM20948_SPI, &REG, 1, 1000);
-	//データを受信
-	HAL_SPI_Receive(ICM20948_SPI, Buffer, Len, 1000);
-}
+	uint8_t TxBuffer[2] = {};
+	uint8_t RxBuffer[2] = {};
 
+	for(uint8_t i=0; i<Len; i++){
+
+		TxBuffer[0] = (uint8_t(REGISTER_ADDR) + i) | 0x80;
+		TxBuffer[1] = 0x00;
+
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
+		HAL_SPI_TransmitReceive(&hspi1, TxBuffer, RxBuffer, 2, 100);
+
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+
+		Buffer[i] = RxBuffer[1];
+	}
+}
